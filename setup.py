81674e01857d21c9cb3cfbe5f60464b1add88997
Alex Parker <alexofparker@gmail.com>
2025-07-04 01:24:21 +0800

    bump: Version increment from 0.1.0 to 0.2.0
    Routine version update reflecting ongoing project development and recent package restructuring

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="git-cam",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     scripts=["git-cam"],
     install_requires=["anthropic>=0.39.0", "colorama>=0.4.6", "pathspec>=0.11.2"],
